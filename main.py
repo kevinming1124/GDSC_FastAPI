0923505170799d9878b6fd8d239fdf0515a9530d
kevinming1124 <kevinming1124@gmail.com>
2023-11-23 16:39:15 +0800

    day3

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from setting.config import get_settings
 
-my_app = FastAPI()
+app = FastAPI()
 
-@my_app.get("/")
-def root():
-    return {"message": "Hello World"}
+@app.get("/")
+def hello_world():
+    return "Hello World"
+
+@app.get("/users/{user_id}")
+def get_users(user_id: int, qry: str = None):
+    return {"user_id": user_id, "query": qry }
+
+@app.get("/infor")
+def get_infor():
+    settings = get_settings()
+    return {
+        "app_name": settings.app_name,
+        "author": settings.author,
+        "app_mode": settings.app_mode,
+        "port": settings.port,
+        "reload": settings.reload,
+        "database_url": settings.database_url
+    }
---
+++ setting/config.py
@@ -0,0 +1,19 @@
+import os
+from functools import lru_cache
+
+from dotenv import load_dotenv
+
+
+@lru_cache()
+def get_settings():
+    load_dotenv( f".env.{os.getenv('APP_MODE')}")
+    return Settings()
+
+class Settings():
+    app_name:str = "iThome2023 FastAPI Tutorial"
+    author:str = "Kevin Ming"
+
+    app_mode: str = os.getenv("APP_MODE")
+    port:int = int(os.getenv("PORT"))
+    reload:bool = bool(os.getenv("RELOAD"))
+    database_url:str = os.getenv("DATABASE_URL")
